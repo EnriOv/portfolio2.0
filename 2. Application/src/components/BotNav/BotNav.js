225c7345c03bec893498f4fa5ff948e2a5a2cbe1
EnriOv <orrantia.en@gmail.com>
2021-06-09 20:45:26 -0600

    Bottom nav bar basic layout.

---
+++ 2. Application/src/components/BotNav/BotNav.js
@@ -0,0 +1,15 @@
+import './BotNav.css'
+
+const BotNav = () => {
+    return (
+        <div className='botnav'>
+            <a href='blank'><i>Icon 1</i></a>
+            <a href='blank'><i>Icon 2</i></a>
+            <a href='blank'><i>Icon 3</i></a>
+            <a href='blank'><i>Icon 4</i></a>
+            <a href='blank'><i>Icon 5</i></a>
+        </div>
+    )
+}
+
+export default BotNav;

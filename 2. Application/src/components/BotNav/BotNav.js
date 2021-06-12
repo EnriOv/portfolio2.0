bbe1459280d56d5419a15f18c2c5f028dfd5f8d3
EnriOv <orrantia.en@gmail.com>
2021-06-12 17:01:58 -0600

    Finished first version of bottom navigation bar for phone view.

--- 2. Application/src/components/BotNav/BotNav.js
+++ 2. Application/src/components/BotNav/BotNav.js
@@ -1,13 +1,19 @@
+import { FiLinkedin } from 'react-icons/fi';
+import IconHome from './assets/IconHome';
+import IconSuitcase from './assets/IconSuitcase';
+import IconGithub from './assets/IconGithub';
+import IconDocument from './assets/IconDocument';
+
 import './BotNav.css'
 
 const BotNav = () => {
     return (
         <div className='botnav'>
-            <a href='blank'><i>Icon 1</i></a>
-            <a href='blank'><i>Icon 2</i></a>
-            <a href='blank'><i>Icon 3</i></a>
-            <a href='blank'><i>Icon 4</i></a>
-            <a href='blank'><i>Icon 5</i></a>
+            <a href='blank' title='Home'><IconHome className='icon-home' /></a>
+            <a href='blank' title='Projects'><IconSuitcase className='icon-suitcase' /></a>
+            <a href='blank' title='GitHub'><IconGithub className='icon-github' /></a>
+            <a href='blank' title='LinkedIn'><FiLinkedin className='icon-linkedin' /></a>
+            <a href='blank' title='Resume'><IconDocument className='icon-resume' /></a>
         </div>
     )
 }

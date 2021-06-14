22d06723f81c3ebc26d8d6f1b984258b68f1b0f2
EnriOv <orrantia.en@gmail.com>
2021-06-14 17:01:24 -0600

    Tags section was added.

--- 2. Application/src/components/ProjectCard/ProjectCard.js
+++ 2. Application/src/components/ProjectCard/ProjectCard.js
@@ -5,9 +5,13 @@ const ProjectCard = () => {
         <div className='project-card'>
             <div className='project-info'>
                 <h3 className='project-title'>Project Card</h3>
-                <p className='abstract'>Absract</p>
+                <p className='abstract'>Sunt voluptate deserunt pariatur cillum dolore ad aute esse elit non labore. Est qui cupidatat voluptate culpa minim dolore excepteur incididunt sunt qui ullamco irure. Dolore do irure qui cupidatat commodo tempor nostrud deserunt anim. Ullamco consectetur elit ex ut laborum ea dolor ea mollit.</p>
                 <div className='tags-section'>
-                    Tags
+                    <div className='tag'>tag</div>
+                    <div className='tag'>tag</div>
+                    <div className='tag'>tag</div>
+                    <div className='tag'>tag</div>
+                    <div className='tag'>tag</div>
                 </div>
             </div>
             <a href='blank' className='demo'>

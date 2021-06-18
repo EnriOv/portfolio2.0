f02a6d769e201729253c286f64c1e5c399926e50
EnriOv <orrantia.en@gmail.com>
2021-06-18 16:01:47 -0600

    Added useEffects and states to handle window resize.

--- 2. Application/src/App.js
+++ 2. Application/src/App.js
@@ -3,10 +3,48 @@ import ProjectCard from './components/ProjectCard/ProjectCard';
 import ProjectInfo from './components/ProjectInfo/ProjectInfo';
 import TopNav from './components/TopNav/TopNav';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [displayType, setdisplayType] = useState('unknown');
+
+  useEffect(() => {
+    setdisplayType(checkWindowSize());
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', checkWindowSize);
+
+    return () => {
+      window.removeEventListener('resize', checkWindowSize);
+    }
+  })
+
+  const checkWindowSize = () => {
+    if(window.innerWidth > 992) {
+      setdisplayType('desktop');
+      return 'desktop';
+    }
+    if(window.innerWidth < 992) {
+      setdisplayType('phone');
+      return 'phone';
+    }
+  }
+
+  const checkDisplayType = () => {
+    switch(displayType) {
+      case 'desktop':
+        return <TopNav />
+      case 'phone':
+        return <BotNav />
+      default:
+        return <ProjectCard />
+    }
+  }
+
   return (
     <div className="App">
-      
+      {checkDisplayType()}
     </div>
   );
 }
